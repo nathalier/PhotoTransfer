29aefba1aec59e0e40aa4177cac8ea746b08eed1
nathalier <nathalier@gmail.com>
2020-07-13 13:53:54 +0100

    add comind line option --noprompt/-y(es) to silently rename without prompting confirmation

--- transfer_rename.py
+++ transfer_rename.py
@@ -1,7 +1,9 @@
+import argparse
 from datetime import datetime
 import os
 from pathlib import Path
 import re
+import sys
 import time
 from settings import settings, DateSource
 
@@ -11,8 +13,6 @@ base_folder = 'PhotoVideo/'
 devices = ['Canon', 'Lenovo2', 'GoPro']
 
 base_path = Path(f'{disk_name}:/{base_folder}')
-
-pretend = True
 
 
 def date_from_file_name(pattern):
@@ -27,37 +27,55 @@ def date_from_meta_info(fn):
 	return customized
 
 
-log = []
-for cam in devices:
-	src_dir = 'tmp_' + cam
-	src_path = base_path / src_dir
-	dest_path = base_path
+def transfer(confirmed=False):
+	log = []
+	for cam in devices:
+		src_dir = 'tmp_' + cam
+		src_path = base_path / src_dir
+		dest_path = base_path
 
-	if settings[cam]['date_source'] == DateSource.CREAT_DATE:
-		apply_fn = date_from_meta_info(os.path.getctime)
-	elif settings[cam]['date_source'] == DateSource.MODIF_DATE:
-		apply_fn = date_from_meta_info(os.path.getmtime)
-	elif settings[cam]['date_source'] == DateSource.FILE_NAME and settings[cam]['pattern']:
-		apply_fn = date_from_file_name(settings[cam]['pattern'])
-	else:
-		raise NotImplementedError('Unknown pattern. Common date extraction is not yet implemented.')
+		if settings[cam]['date_source'] == DateSource.CREAT_DATE:
+			apply_fn = date_from_meta_info(os.path.getctime)
+		elif settings[cam]['date_source'] == DateSource.MODIF_DATE:
+			apply_fn = date_from_meta_info(os.path.getmtime)
+		elif settings[cam]['date_source'] == DateSource.FILE_NAME and settings[cam]['pattern']:
+			apply_fn = date_from_file_name(settings[cam]['pattern'])
+		else:
+			raise NotImplementedError('Unknown pattern. Common date extraction is not yet implemented.')
 
-	files = []
-	for e in settings[cam]['extensions']:
-		files += list(src_path.glob(f'*.{e}'))
+		files = []
+		for e in settings[cam]['extensions']:
+			files += list(src_path.glob(f'*.{e}'))
 
-	for x in files:
-		try:
-			time_prefix = apply_fn(x)
-			new_fname = settings[cam]['transform'](time_prefix, x)
-			new_path = dest_path / new_fname
-			if not pretend:
-				x.rename(new_path)
-			log.append(f'{x}, {new_path}\n')
-		except Exception as e:
-			print(x.name + ' error: ', e)
+		for x in files:
+			try:
+				time_prefix = apply_fn(x)
+				new_fname = settings[cam]['transform'](time_prefix, x)
+				new_path = dest_path / new_fname
+				if confirmed:
+					x.rename(new_path)
+				else:
+					print(f'{x} \t->\t {new_path}')
+				log.append(f'{x}, {new_path}\n')
+			except Exception as e:
+				print(x.name + ' error: ', e)
 
-if log:
-	with open(f'log_{time.strftime("%y%m%d_%H%M%S")}{"_pretend" if pretend else ""}.csv',
-	          'w', encoding='utf-8') as f:
-		f.writelines(log)
+	if log:
+		with open(f'log_{time.strftime("%y%m%d_%H%M%S")}{"" if confirmed else "_pretend"}.csv',
+		          'w', encoding='utf-8') as f:
+			f.writelines(log)
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--noprompt', '-y', action='store_true',
+	                    help='all files will be renamed without output and confirmation prompt')
+	args = parser.parse_args()
+	if not args.noprompt:
+		print('The following files will be renamed:')
+		transfer(confirmed=False)
+		perform = input('Do you wish to continue? [N]/y: ')
+		if perform.lower() != 'y':
+			print('No files were renamed')
+			sys.exit(0)
+	transfer(confirmed=True)
